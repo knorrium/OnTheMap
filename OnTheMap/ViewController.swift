41a0300c7a4c96038c6bb54a70239815e50aff4b
Felipe Knorr Kuhn <git@knorrium.info>
2015-12-19 09:20:56 -0800

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Felipe Kuhn on 12/19/15.
+//  Copyright © 2015 Knorrium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
