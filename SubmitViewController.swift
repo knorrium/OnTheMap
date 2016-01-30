8b563defef321a5fded7c775e8d3ce32083e6c7f
Felipe Knorr Kuhn <git@knorrium.info>
2016-01-30 10:47:10 -0800

    look up the geocoded information and drop a pin on the map with it

--- SubmitViewController.swift
+++ SubmitViewController.swift
@@ -9,13 +9,52 @@
 import UIKit
 import MapKit
 
-class SubmitViewController: UIViewController {
+class SubmitViewController: UIViewController, MKMapViewDelegate {
 
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var txtLocation: UITextField!
     
     @IBAction func submitLocation(sender: AnyObject) {
+        if (txtLocation.text != nil && txtLocation.text != "") {
+            var geocoder = CLGeocoder()
+            geocoder.geocodeAddressString(txtLocation.text!, completionHandler: {(placemarks: [CLPlacemark]?, error: NSError?) -> Void in
+                
+              if error == nil && placemarks!.count > 0 {
+                    let placemark = placemarks![0]
+                    let location = placemark.location!
+                    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 20000, 20000)
+                    self.mapView.setRegion(coordinateRegion, animated: true)
+                    var studentInfo = StudentInformation(dictionary: [
+                        "uniqueKey": self.appDelegate.loggedUser.uniqueKey,
+                        "firstName": "Hardcoded First",
+                        "lastName": "Hardcoded Last",
+                        "latitude": location.coordinate.latitude,
+                        "longitude": location.coordinate.longitude,
+                        "mediaURL": ""
+                    ])
+                
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = location.coordinate
+                    annotation.title = studentInfo.firstName
+                    annotation.subtitle = studentInfo.mediaURL
+
+                    self.mapView.removeAnnotations(self.mapView.annotations)
+                    self.mapView.addAnnotation(annotation)
+                }
+              else {
+                let alertController = UIAlertController(title: "On The Map", message:
+                    "There was an error while trying to fetch the address information", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                })
+
+                }
+            })
+        }
         
     }
     
@@ -24,6 +63,7 @@ class SubmitViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Submit Location View")
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
